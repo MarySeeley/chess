72bb51c758fc66ec1eddef2ade48887986a7cb07
MarySeeley <mary.seeley@outlook.com>
2024-01-16 13:57:02 -0700

    Fixed if statements in PawnMove

--- shared/src/main/java/chess/PawnMove.java
+++ shared/src/main/java/chess/PawnMove.java
@@ -17,28 +17,42 @@ public class PawnMove {
     int startRow = starting.getRow();
     int startCol = starting.getColumn();
     Collection<ChessMove> options=new ArrayList<>();
+    Collection<ChessPiece.PieceType> promotionType = new ArrayList<>();
+    promotionType.add(ChessPiece.PieceType.QUEEN);
+    promotionType.add(ChessPiece.PieceType.ROOK);
+    promotionType.add(ChessPiece.PieceType.KNIGHT);
+    promotionType.add(ChessPiece.PieceType.BISHOP);
     if(piece.getTeamColor() == ChessGame.TeamColor.WHITE){
       ChessPosition whiteForwardPos = new ChessPosition(startRow+1, startCol);
       if(board.getPiece(whiteForwardPos)==null){
         if(startRow+1 == 8) {
+          for(ChessPiece.PieceType n : promotionType){
+            options.add(new ChessMove(starting, whiteForwardPos, n));
+          }
           //loop through each piece as an option to replace???
-          options.add(new ChessMove(starting, whiteForwardPos, ChessPiece.PieceType.KNIGHT));
-          options.add(new ChessMove(starting, whiteForwardPos, ChessPiece.PieceType.QUEEN));
-          options.add(new ChessMove(starting, whiteForwardPos, ChessPiece.PieceType.ROOK));
-          options.add(new ChessMove(starting, whiteForwardPos, ChessPiece.PieceType.BISHOP));
+//          options.add(new ChessMove(starting, whiteForwardPos, ChessPiece.PieceType.KNIGHT));
+//          options.add(new ChessMove(starting, whiteForwardPos, ChessPiece.PieceType.QUEEN));
+//          options.add(new ChessMove(starting, whiteForwardPos, ChessPiece.PieceType.ROOK));
+//          options.add(new ChessMove(starting, whiteForwardPos, ChessPiece.PieceType.BISHOP));
           ChessPosition rightDiagonalCapture=new ChessPosition(startRow + 1, startCol + 1);
           if (board.getPiece(rightDiagonalCapture) != null && board.getPiece(rightDiagonalCapture).getTeamColor() == ChessGame.TeamColor.BLACK) {
-            options.add(new ChessMove(starting, rightDiagonalCapture, ChessPiece.PieceType.KNIGHT));
-            options.add(new ChessMove(starting, rightDiagonalCapture, ChessPiece.PieceType.QUEEN));
-            options.add(new ChessMove(starting, rightDiagonalCapture, ChessPiece.PieceType.ROOK));
-            options.add(new ChessMove(starting, rightDiagonalCapture, ChessPiece.PieceType.BISHOP));
+            for(ChessPiece.PieceType n : promotionType){
+              options.add(new ChessMove(starting, rightDiagonalCapture, n));
+            }
+//            options.add(new ChessMove(starting, rightDiagonalCapture, ChessPiece.PieceType.KNIGHT));
+//            options.add(new ChessMove(starting, rightDiagonalCapture, ChessPiece.PieceType.QUEEN));
+//            options.add(new ChessMove(starting, rightDiagonalCapture, ChessPiece.PieceType.ROOK));
+//            options.add(new ChessMove(starting, rightDiagonalCapture, ChessPiece.PieceType.BISHOP));
           }
           ChessPosition leftDiagonalCapture=new ChessPosition(startRow + 1, startCol - 1);
           if (board.getPiece(leftDiagonalCapture) != null && board.getPiece(leftDiagonalCapture).getTeamColor() == ChessGame.TeamColor.BLACK) {
-            options.add(new ChessMove(starting, leftDiagonalCapture, ChessPiece.PieceType.KNIGHT));
-            options.add(new ChessMove(starting, leftDiagonalCapture, ChessPiece.PieceType.QUEEN));
-            options.add(new ChessMove(starting, leftDiagonalCapture, ChessPiece.PieceType.ROOK));
-            options.add(new ChessMove(starting, leftDiagonalCapture, ChessPiece.PieceType.BISHOP));
+            for(ChessPiece.PieceType n : promotionType){
+              options.add(new ChessMove(starting, leftDiagonalCapture, n));
+            }
+//            options.add(new ChessMove(starting, leftDiagonalCapture, ChessPiece.PieceType.KNIGHT));
+//            options.add(new ChessMove(starting, leftDiagonalCapture, ChessPiece.PieceType.QUEEN));
+//            options.add(new ChessMove(starting, leftDiagonalCapture, ChessPiece.PieceType.ROOK));
+//            options.add(new ChessMove(starting, leftDiagonalCapture, ChessPiece.PieceType.BISHOP));
           }
         }
         else {
@@ -50,13 +64,13 @@ public class PawnMove {
         }
       }
       ChessPosition rightDiagonal = new ChessPosition(startRow+1, startCol+1);
-      if(startRow+1 <=8 && startCol+1 <=8){
+      if(startRow+1 <=8 && startCol+1 <=8&& startRow+1 !=8){
         if(board.getPiece(rightDiagonal) != null && board.getPiece(rightDiagonal).getTeamColor() == ChessGame.TeamColor.BLACK){
           options.add(new ChessMove(starting, rightDiagonal, null));
         }
       }
       ChessPosition leftDiagonal = new ChessPosition(startRow+1, startCol-1);
-      if(startRow+1 <=8 && startCol-1 >=1){
+      if(startRow+1 <=8 && startCol-1 >=1 && startRow+1 !=8){
         if(board.getPiece(leftDiagonal) != null && board.getPiece(leftDiagonal).getTeamColor() == ChessGame.TeamColor.BLACK){
           options.add(new ChessMove(starting, leftDiagonal, null));
         }
@@ -67,12 +81,27 @@ public class PawnMove {
     else{
       ChessPosition blackForwardPos = new ChessPosition(startRow-1, startCol);
       if(board.getPiece(blackForwardPos)==null){
-        if(startRow-1 == 1){
+        if(startRow-1 == 1) {
+          for (ChessPiece.PieceType n : promotionType) {
+            options.add(new ChessMove(starting, blackForwardPos, n));
+          }
           //loop through each piece as an option to replace???
-          options.add(new ChessMove(starting, blackForwardPos, ChessPiece.PieceType.QUEEN));
-          options.add(new ChessMove(starting, blackForwardPos, ChessPiece.PieceType.ROOK));
-          options.add(new ChessMove(starting, blackForwardPos, ChessPiece.PieceType.BISHOP));
-          options.add(new ChessMove(starting, blackForwardPos, ChessPiece.PieceType.KNIGHT));
+//          options.add(new ChessMove(starting, blackForwardPos, ChessPiece.PieceType.ROOK));
+//          options.add(new ChessMove(starting, blackForwardPos, ChessPiece.PieceType.QUEEN));
+//          options.add(new ChessMove(starting, blackForwardPos, ChessPiece.PieceType.BISHOP));
+//          options.add(new ChessMove(starting, blackForwardPos, ChessPiece.PieceType.KNIGHT));
+          ChessPosition rightDiagonal=new ChessPosition(startRow - 1, startCol + 1);
+          ChessPosition leftDiagonal=new ChessPosition(startRow - 1, startCol - 1);
+          if (board.getPiece(rightDiagonal) != null && board.getPiece(rightDiagonal).getTeamColor() == ChessGame.TeamColor.WHITE) {
+            for (ChessPiece.PieceType n : promotionType) {
+              options.add(new ChessMove(starting, rightDiagonal, n));
+            }
+          }
+          else if (board.getPiece(leftDiagonal) != null && board.getPiece(leftDiagonal).getTeamColor() == ChessGame.TeamColor.WHITE) {
+            for (ChessPiece.PieceType n : promotionType) {
+              options.add(new ChessMove(starting, leftDiagonal, n));
+            }
+          }
         }
         else{
           options.add(new ChessMove(starting, blackForwardPos, null));
@@ -82,15 +111,14 @@ public class PawnMove {
           options.add(new ChessMove(starting, doubleMove, null));
         }
       }
-
       ChessPosition rightDiagonal = new ChessPosition(startRow-1, startCol+1);
-      if(startRow-1 <=8 && startCol+1 <=8) {
+      if(startRow-1 <=8 && startCol+1 <=8 && startRow-1 != 1) {
         if (board.getPiece(rightDiagonal) != null && board.getPiece(rightDiagonal).getTeamColor() == ChessGame.TeamColor.WHITE) {
           options.add(new ChessMove(starting, rightDiagonal, null));
         }
       }
       ChessPosition leftDiagonal = new ChessPosition(startRow-1, startCol-1);
-      if(startRow-1 >=1 && startCol-1 >=1){
+      if(startRow-1 >=1 && startCol-1 >=1 && startRow-1 != 1){
         if(board.getPiece(leftDiagonal) != null && board.getPiece(leftDiagonal).getTeamColor() == ChessGame.TeamColor.WHITE){
           options.add(new ChessMove(starting, leftDiagonal, null));
         }
