5cf539a3efef9070b49cc5b8bbb1dc1237f97aae
MarySeeley <mary.seeley@outlook.com>
2024-02-03 18:35:36 -0700

    Stalemate check to see current team

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -209,7 +209,10 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        if(teamColor != getTeamTurn()){
+            return false;
+        }
+        return true;
     }
 
     /**
